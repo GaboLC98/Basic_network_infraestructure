bc7971800fbc90ea5b1dd7326483acf48dd16359
Gabriel Lopez Calderon <gabriel.lopezcalderon@protonmail.com>
2022-06-08 17:36:38 -0300

    tfvars, variables.tf, locals.tf and private and public subnet created

--- resources.tf
+++ resources.tf
@@ -6,7 +6,7 @@ resource "aws_vpc" "vpc_1" {
     enable_dns_hostnames = "true"
 
     tags = {
-      "Name" = "VPC-1"
+      Name = "VPC-1-${local.common_tags.Project}"
     }
 }
 
@@ -15,18 +15,32 @@ resource "aws_internet_gateway" "igw" {
     vpc_id = aws_vpc.vpc_1.id
 
     tags = {
-      "Name" = "igw-VPC-1"
+      Name = "igw-VPC-1-${local.common_tags.Project}"
     }  
 }
 
-# Subnet 1
-resource "aws_subnet" "subnet_1" {
+# Public Subnet
+resource "aws_subnet" "public_subnets" {
+    count = 3
     vpc_id = aws_vpc.vpc_1.id
-    cidr_block = "10.0.0.0/24"
+    cidr_block = var.subnet_public_cidr_block[count.index]
+    availability_zone = var.av_zones[count.index]
     map_public_ip_on_launch = "true"
 
     tags = {
-        Name = "Subnet_public-VPC-1"
+        Name = "Public_subnet-${count.index}-VPC-1-${local.common_tags.Project}"
+    }  
+}
+# Private Subnet
+resource "aws_subnet" "private_subnets" {
+    count = 3
+    vpc_id = aws_vpc.vpc_1.id
+    cidr_block = var.subnet_private_cidr_block[count.index]
+    availability_zone = var.av_zones[count.index]
+    map_public_ip_on_launch = "false"
+
+    tags = {
+        Name = "Private_subnet-${count.index}-VPC-1-${local.common_tags.Project}"
     }  
 }
 
@@ -40,8 +54,9 @@ resource "aws_route_table" "rt_1" {
     }
 }
 # Route table association
-resource "aws_route_table_association" "rt_subnet_1" {
-    subnet_id = aws_subnet.subnet_1.id
+resource "aws_route_table_association" "rt_subnet" {
+  count = 3
+    subnet_id = aws_subnet.public_subnets[count.index].id
     route_table_id = aws_route_table.rt_1.id  
 }
 
@@ -55,34 +70,42 @@ resource "aws_security_group" "security_group_http" {
         description = "HTTP"
         from_port = 80
         to_port = 80
-        cidr_blocks = [ "0.0.0.0/0" ]
         protocol = "tcp"
-    }  
-    ingress = {
+        cidr_blocks = [ "0.0.0.0/0" ]
+    }
+
+    ingress {
       description = "HTTPS"
       from_port = 442
       to_port = 442
-      cidr_blocks = [ "0.0.0.0/0" ]
       protocol = "tcp"
+      cidr_blocks = [ "0.0.0.0/0" ]
       }
     
-    egress = {
+    egress {
       from_port = 0
       to_port = 0
       cidr_blocks = [ "0.0.0.0/0" ]
-      protocol = -1
+      protocol = "-1"
     }
 
     tags = {
-      "Name" = "HTTP-Security_group"
+      Name = "HTTP-Security_group"
     }
 }
+/* <--- Delete for EC2 instance creation
 
 # EC2
 resource "aws_instance" "EC2_1" {
+    count = 3 
     ami = "" #Choice -> Have to select image
     instance_type = "" #Choice -> Have to select instance type (t2.micro, t2.nano, etc)
-    subnet_id = aws_subnet.subnet_1.id
+    subnet_id = aws_subnet.public_subnets[count.index].id
     vpc_security_group_ids = [ aws_security_group.security_group_http.id ]
-  
+
+    tags = {
+      Name = "EC2-${count.index}-${local.common_tags.Project}"
+    }
 }
+
+*/ #<--- Delete for EC2 instance creation
---
+++ terraform.tfvars
@@ -0,0 +1,3 @@
+subnet_public_cidr_block = [ "10.0.0.0/24","10.0.1.0/24","10.0.2.0/24" ]
+av_zones = [ "us-east-1a","us-east-1b","us-east-1c" ]
+subnet_private_cidr_block = [ "10.0.10.0/24","10.0.11.0/24","10.0.12.0/24" ]
