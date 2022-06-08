49f8bf859d368ab2ea4535623314f6e70f3b6b0d
Gabriel Lopez Calderon <gabriel.lopezcalderon@protonmail.com>
2022-06-08 19:42:26 -0300

    Created instance.tf

--- resources.tf
+++ resources.tf
@@ -1,5 +1,4 @@
-## AWS RESOURCES ##
-# Networking #
+# NETWORK #
 # vpc
 resource "aws_vpc" "vpc_1" {
     cidr_block = "10.0.0.0/16"
@@ -93,19 +92,3 @@ resource "aws_security_group" "security_group_http" {
       Name = "HTTP-Security_group"
     }
 }
-/* <--- Delete for EC2 instance creation
-
-# EC2
-resource "aws_instance" "EC2_1" {
-    count = 3 
-    ami = "" #Choice -> Have to select image
-    instance_type = "" #Choice -> Have to select instance type (t2.micro, t2.nano, etc)
-    subnet_id = aws_subnet.public_subnets[count.index].id
-    vpc_security_group_ids = [ aws_security_group.security_group_http.id ]
-
-    tags = {
-      Name = "EC2-${count.index}-${local.common_tags.Project}"
-    }
-}
-
-*/ #<--- Delete for EC2 instance creation
