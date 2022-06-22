dfd98ae304dd917b4d32c0336d03372798ced67d
Gabriel Lopez Calderon <gabriel.lopezcalderon@protonmail.com>
2022-06-22 14:11:00 -0300

    Added dynamic block

--- variables.tf
+++ variables.tf
@@ -1,21 +1,36 @@
 variable "subnets_quantity" {
-  type = number
+    type = number  
 }
 
-variable "instance_quantity" {
-  type = number
+variable instance_quantity {
+    type = number
 }
 
-variable "subnet_private_cidr_block" {
-  type = list(string)
+variable subnet_private_cidr_block {
+    type = list(string)
 }
 
 variable "av_zones" {
-  type = list(string)
+    type = list(string)  
 }
 
 variable "VPC_cidr_block" {
-  type        = string
-  description = "CIDR block for VPC"
-  default     = "10.0.0.0/16"
+    type = string
+    description = "CIDR block for VPC"
+    default = "10.0.0.0/16"
+}
+
+variable "security" {
+    default =  [{
+        description = "HTTP"
+        port = 80
+        protocol = "tcp"
+        cidr_block = [ "0.0.0.0/0" ]
+    },
+    {
+        description = "HTTPS"
+        port = 442
+        protocol = "tcp"
+        cidr_block = [ "0.0.0.0/0" ]
+    }]  
 }
