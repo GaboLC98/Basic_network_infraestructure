e094858851d74b22f23d9565470d38e41aa84745
Gabriel Lopez Calderon <gabriel.lopezcalderon@protonmail.com>
2022-06-22 15:38:21 -0300

    Comments added

--- locals.tf
+++ locals.tf
@@ -1,6 +1,7 @@
+#Local variables
 locals {
   common_tags = {
-    Project     = "basic_network_infraestructure"
-    Envirioment = "Dev"
+    Project     = "basic_network_infraestructure" #Project name
+    Envirioment = "Dev" #Envirioment name
   }
 }
