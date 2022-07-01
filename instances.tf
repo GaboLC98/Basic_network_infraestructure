6576b2a4c7a44bfe82ad9c216e7695efc5a29112
Gabriel Lopez Calderon <gabriel.lopezcalderon@protonmail.com>
2022-07-01 11:54:31 -0300

    instance enabled

--- instances.tf
+++ instances.tf
@@ -1,11 +1,9 @@
-/* <--- Delete for EC2 instance creation in each public subnet
-
 
 # EC2
 resource "aws_instance" "EC2_1" {
     count = var.instance_quantity
-    ami = "" #Choice -> Have to select image
-    instance_type = "" #Choice -> Have to select instance type (t2.micro, t2.nano, etc)
+    ami = "ami-052efd3df9dad4825" #Choice -> Have to select image
+    instance_type = "t2.micro" #Choice -> Have to select instance type (t2.micro, t2.nano, etc)
     subnet_id = aws_subnet.public_subnets[count.index].id
     vpc_security_group_ids = [ aws_security_group.security_group_http.id ]
 
@@ -13,6 +11,3 @@ resource "aws_instance" "EC2_1" {
       Name = "EC2-${count.index}-${local.common_tags.Project}"
     }
 }
-
-
-Delete for EC2 instance creation in each public subnet ---> */
